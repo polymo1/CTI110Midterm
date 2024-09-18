416e4ee953cca274383a7795978b589e51ddd075
Wesley Bryie <wes@wesbryie.com>
2024-09-18 13:05:49 -0400

    .

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/*
+    Copyright (c) 2024 Wesley Bryie. All Rights Reserved.
+    index.js
+    CTI 110 LB1
+    Midterm Project
+*/
+
+print("This is the developer console. You have no business being in here.")
